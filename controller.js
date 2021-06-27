e1efc7e94dcd5c70901088d11e26862572d57a6a
HifzonHifzon <Hifzon746@gmail.com>
2021-06-27 10:37:48 +0700

    all tampil & by id

--- controller.js
+++ controller.js
@@ -7,3 +7,30 @@ var koneksi = require('./koneksi');
 exports.index = function(req, res) {
     response.ok('Aplikasi aku berjalan....!!!', res);
 }
+
+/* Menampilkan semua data mahasiswa */
+exports.tampil = function(req, res) {
+    // response.ok('Ini Halaman Buku', res);
+    koneksi.query("SELECT * FROM mahasiswa", function(error, rows, field){
+        if (error) {
+            koneksi.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    })
+}
+
+/* Menampilkan data mahasiswa berdasarkan ID*/
+exports.tamilbyid = function(req, res){
+    let id = req.params.id;
+    let nama = req.params.nama;
+    
+    koneksi.query("SELECT * FROM mahasiswa where id  = ? and nama = ?", [id,nama], function(error, rows, field){
+        if (error){
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    })
+
+}
--- routes.js
+++ routes.js
@@ -5,4 +5,16 @@ module.exports = function(app) {
 
     app.route('/')
         .get(jsonku.index);
+
+        app.route('/buku')
+        .get(jsonku.tampil);
+
+        app.route('/buku/:id/:nama').get(jsonku.tamilbyid);
 }
+
+// module.exports = function(app) {
+//     var jsonku = require('./controller');
+
+//     app.route('/buku')
+//         .get(jsonku.buku);
+// }
